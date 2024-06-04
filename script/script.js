132a6654721121fadc55a58620382ef2599a7f61
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-04 15:53:26 +0200

    true

---
+++ script/script.js
@@ -0,0 +1,13 @@
+let values = [true, true, false, true];
+let button = document.querySelector('button');
+let answer = document.querySelector('span')
+let value = 0
+
+button.addEventListener('click', ()=>{
+    for (const bool of values) {
+        if(bool == true){
+            value++
+        }
+    }
+    answer.innerText = value;
+})
