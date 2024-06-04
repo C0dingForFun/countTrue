c050232574f57631ee8b7b6bfa3c31dc8d55bf9e
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-04 15:59:17 +0200

    true

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ let answer = document.querySelector('span')
 let value = 0
 
 button.addEventListener('click', ()=>{
-    for (const bool of values) {
+    for (const bool of values) { //loop through the array values
         if(bool == true){
             value++
         }
